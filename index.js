508579496acb12c1ab13bdaf8b35aaa2ad8a30f3
Quentin <you@example.com>
2024-06-10 12:02:50 +0200

    prettier changes

--- index.js
+++ index.js
@@ -1,108 +1,98 @@
-
-console.log('coucou 55');
 // document.addEventListener('DOMContentLoaded', function () {});
 
-console.log('coucou');
-const closeBtn = document.querySelector('.close-button');
-const hamburger = document.querySelector('#hamburger');
+const closeBtn = document.querySelector(".close-button");
+const hamburger = document.querySelector("#hamburger");
 
-closeBtn.addEventListener('click', function () {
-    hamburger.checked = false; // Décoche la case à cocher
+closeBtn.addEventListener("click", function () {
+  hamburger.checked = false; // Décoche la case à cocher
 });
 
-    
 // var text = document.getElementById('text');
 // console.log(text);
 // console.log(text.innerText);
 let quantite = 1;
 
-const quantiteHTML = document.getElementById('quantite')
-const priceHTML = document.getElementById('price')
+const quantiteHTML = document.getElementById("quantite");
+const priceHTML = document.getElementById("price");
 
 // Init the price
-if (priceHTML){
-    priceHTML.innerText = '0';
+if (priceHTML) {
+  priceHTML.innerText = "0";
 }
 
 // Init the quantite
 if (quantiteHTML) {
-    quantiteHTML.innerText = quantite;
+  quantiteHTML.innerText = quantite;
 }
 
 let pricePerSize = 19.99;
 
 function calculAndUpdatePrice() {
-    // toString = Transform into string (permet de manipuler une chaîne de caractères)
-    if (priceHTML){
-        priceHTML.innerText = (pricePerSize * quantite).toFixed(2).toString();
-
-    }
+  // toString = Transform into string (permet de manipuler une chaîne de caractères)
+  if (priceHTML) {
+    priceHTML.innerText = (pricePerSize * quantite).toFixed(2).toString();
+  }
 }
-calculAndUpdatePrice()
+calculAndUpdatePrice();
 
-const lessQuantiteButtonHTML = document.getElementById('less-quantite');
+const lessQuantiteButtonHTML = document.getElementById("less-quantite");
 
 if (lessQuantiteButtonHTML) {
-    lessQuantiteButtonHTML.addEventListener('click', () => {
-        console.log('---------- [less-quantite] ----------',quantite);
-    
-        if (quantite > 1) {
-            console.log('is more than 1');
-            quantite = quantite - 1;
-            quantiteHTML.innerText = quantite;
-            calculAndUpdatePrice();
-        }
-    
-    
-        console.log('Price = ' , quantite)
-    });
+  lessQuantiteButtonHTML.addEventListener("click", () => {
+    console.log("---------- [less-quantite] ----------", quantite);
+
+    if (quantite > 1) {
+      console.log("is more than 1");
+      quantite = quantite - 1;
+      quantiteHTML.innerText = quantite;
+      calculAndUpdatePrice();
+    }
+
+    console.log("Price = ", quantite);
+  });
 }
 
-const moreQuantiteButtonHTML = document.getElementById('more-quantite');
+const moreQuantiteButtonHTML = document.getElementById("more-quantite");
 
-if (moreQuantiteButtonHTML){
-    moreQuantiteButtonHTML.addEventListener('click', () => {
-        console.log('---------- [more-quantite] ----------',quantite);
-    
-        quantite = quantite + 1;
-        quantiteHTML.innerText = quantite;
-        calculAndUpdatePrice();
-    
-        console.log('Price = ' , quantite)
-    });
+if (moreQuantiteButtonHTML) {
+  moreQuantiteButtonHTML.addEventListener("click", () => {
+    console.log("---------- [more-quantite] ----------", quantite);
+
+    quantite = quantite + 1;
+    quantiteHTML.innerText = quantite;
+    calculAndUpdatePrice();
+
+    console.log("Price = ", quantite);
+  });
 }
 
-const selectSizeHtml = document.getElementById('select-size');
+const selectSizeHtml = document.getElementById("select-size");
 
+if (selectSizeHtml) {
+  selectSizeHtml.addEventListener("change", () => {
+    console.log("selectSizeHtml.value =>", selectSizeHtml.value);
 
+    if (selectSizeHtml.value === "1.5") {
+      pricePerSize = 19.99;
+    } else {
+      pricePerSize = 27.99;
+    }
+    console.log(pricePerSize);
 
-if(selectSizeHtml) {
-    selectSizeHtml.addEventListener('change', () => {
-        console.log('selectSizeHtml.value =>', selectSizeHtml.value);
-    
-        if (selectSizeHtml.value === '1.5') {
-            pricePerSize = 19.99
-        } else {
-            pricePerSize = 27.99
-        }
-        console.log(pricePerSize)
-    
-    
-        calculAndUpdatePrice();
-    
-    })
+    calculAndUpdatePrice();
+  });
 }
 
 function calculAndUpdatePrice() {
-    const price = (pricePerSize * quantite).toFixed(2);
-    if(priceHTML) {
-        // Met à jour le contenu du rond-vert avec le prix calculé et le signe €
-        priceHTML.innerText = price + '€';
-    }
-    // const green = document.querySelector('.green-circle span')
-    // if (green) {
-    //     green.innerText = price + '€';
-    // }
+  const price = (pricePerSize * quantite).toFixed(2);
+  if (priceHTML) {
+    // Met à jour le contenu du rond-vert avec le prix calculé et le signe €
+    priceHTML.innerText = price + "€";
+  }
+  // const green = document.querySelector('.green-circle span')
+  // if (green) {
+  //     green.innerText = price + '€';
+  // }
 }
 
 calculAndUpdatePrice();
@@ -116,4 +106,3 @@ Il définit une fonction calculAndUpdatePrice qui calcule le prix en multipliant
 Il ajoute des gestionnaires d'événements pour les boutons "less-quantite" et "more-quantite" pour augmenter ou diminuer la quantité et mettre à jour le prix en conséquence.
 Il ajoute un gestionnaire d'événements pour l'élément select-size qui met à jour pricePerSize en fonction de la sélection.
 Il réutilise la fonction calculAndUpdatePrice pour mettre à jour le prix dans un élément HTML avec la classe green-circle en ajoutant le signe €. */
-
